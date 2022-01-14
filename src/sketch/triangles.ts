b792f9068c060f6d7e556b15a27a7b9b924d4219
Teresa Pelinski <teresapelinski@gmail.com>
2022-01-14 21:02:43 +0000

    lil fix but did it the other way round lol sorry

--- src/sketch/triangles.ts
+++ src/sketch/triangles.ts
@@ -61,36 +61,34 @@ class Triangle {
 		return intersections
 	}
 	isPointInsideOfTriangle = (point: Point): boolean => {
-		return collidePointPoly(point, this.vertices)
-	}
-}
+		var collision = false
 
-const collidePointPoly = (p: Point, vertices: [Point, Point, Point]) => {
-	var collision = false
+		// go through each of the vertices, plus the next vertex in the list
+		var next = 0
+		for (var current = 0; current < this.vertices.length; current++) {
+			// get next vertex in list if we've hit the end, wrap around to 0
+			next = current + 1
+			if (next === this.vertices.length) next = 0
 
-	// go through each of the vertices, plus the next vertex in the list
-	var next = 0
-	for (var current = 0; current < vertices.length; current++) {
-		// get next vertex in list if we've hit the end, wrap around to 0
-		next = current + 1
-		if (next === vertices.length) next = 0
+			// get the PVectors at our current position this makes our if statement a little cleaner
+			var vc = this.vertices[current] // c for "current"
+			var vn = this.vertices[next] // n for "next"
 
-		// get the PVectors at our current position this makes our if statement a little cleaner
-		var vc = vertices[current] // c for "current"
-		var vn = vertices[next] // n for "next"
-
-		// compare position, flip 'collision' variable back and forth
-		if (
-			vc &&
-			vn &&
-			((vc[1] >= p[1] && vn[1] < p[1]) || (vc[1] < p[1] && vn[1] >= p[1])) &&
-			p[0] < ((vn[0] - vc[0]) * (p[1] - vc[1])) / (vn[1] - vc[1]) + vc[0]
-		) {
-			collision = !collision
+			// compare position, flip 'collision' variable back and forth
+			if (
+				vc &&
+				vn &&
+				((vc[1] >= point[1] && vn[1] < point[1]) ||
+					(vc[1] < point[1] && vn[1] >= point[1])) &&
+				point[0] < ((vn[0] - vc[0]) * (point[1] - vc[1])) / (vn[1] - vc[1]) + vc[0]
+			) {
+				collision = !collision
+			}
 		}
+		return collision
 	}
-	return collision
 }
+
 const calculateAreaOfTriangle = (v0: Point, v1: Point, v2: Point): number =>
 	Math.abs((v0[0] * (v1[1] - v2[1]) + v1[0] * (v2[1] - v0[1]) + v2[0] * (v0[1] - v1[1])) / 2.0)
 
@@ -111,11 +109,16 @@ const collideLineLine = (v: Line, w: Line): [number, number] | null => {
 	}
 }
 
-// const isInside = (point: Point, triangle: Triangle): boolean => {
-// 	const A1: number = calculateAreaOfTriangle(point, triangle.vertices[0], triangle.vertices[1])
-// 	const A2: number = calculateAreaOfTriangle(point, triangle.vertices[1], triangle.vertices[2])
-// 	const A3: number = calculateAreaOfTriangle(point, triangle.vertices[2], triangle.vertices[0])
-// 	return triangle.area - A1 + A2 + A3 < 0.01
+// const getIdxOfClosestPointToVertex = (vertex: Point, arrayOfPoints: Point[]): number => {
+// 	let distances: number[] = []
+
+// 	arrayOfPoints.forEach((pointB: Point) => {
+// 		distances.push(
+// 			Math.sqrt(Math.pow(vertex[0] - pointB[0], 2) + Math.pow(vertex[1] - pointB[1], 2))
+// 		)
+// 	})
+
+// 	return distances.indexOf(Math.max.apply(Math, distances))
 // }
 
 const sketch = (p5: any) => {
@@ -167,6 +170,36 @@ const sketch = (p5: any) => {
 			p5.strokeWeight(5)
 			p5.stroke('red')
 			intersections.forEach((pointsOnLine: any, idx: number) => {
+				// if (pointsOnLine.length == 0) {
+				// 	p5.strokeWeight(2)
+				// 	p5.stroke('green')
+				// 	triangles[0].vertices.forEach((vertex) => {
+				// 		if (
+				// 			!triangles[0].isPointInsideOfTriangle(vertex) &&
+				// 			triangles[0].lines[idx]?.includes(vertex)
+				// 		) {
+				// 			let line = triangles[0].lines[idx]
+				// 			line && p5.line(...line[0], ...line[1])
+				// 		}
+				// 	})
+				// }
+				console.log(triangles[0].lines[idx])
+
+				if (pointsOnLine.length == 0) {
+					let [a, b] = triangles[0].lines[idx] || [
+						[0, 0],
+						[0, 0],
+					]
+					if (
+						triangles[1].isPointInsideOfTriangle(a) &&
+						triangles[1].isPointInsideOfTriangle(b)
+					) {
+						p5.strokeWeight(2)
+						p5.stroke('blue')
+						p5.line(...a, ...b)
+					}
+				}
+
 				// case 2 points on same line
 
 				if (pointsOnLine.length == 2) {
@@ -177,6 +210,17 @@ const sketch = (p5: any) => {
 					p5.stroke('red')
 					p5.point(...pointsOnLine[0])
 					p5.point(...pointsOnLine[1])
+					// draw closest point
+					// triangles[0].vertices.forEach((vertex) => {
+					// 	if (triangles[0].lines[idx]?.includes(vertex)) {
+					// 		p5.strokeWeight(2)
+					// 		p5.stroke('green')
+					// 		p5.line(
+					// 			...vertex,
+					// 			...pointsOnLine[getIdxOfClosestPointToVertex(vertex, pointsOnLine)]
+					// 		)
+					// 	}
+					// })
 				}
 				// case vertex inside triangle0
 				if (pointsOnLine.length == 1) {
@@ -196,104 +240,6 @@ const sketch = (p5: any) => {
 				}
 			})
 		}
-
-		// triangle 1
-		// let triangle1_points: Point[] = []
-		// triangles[1].lines.forEach((line: Line) => {
-		// 	// console.log(!isInside(line[0], triangles[0]))
-		// 	if (!isInside(line[0], triangles[0])) {
-		// 		triangle1_points.push(line[0])
-		// 	}
-		// 	for (let line_prime of triangles[0].lines) {
-		// 		let intersection: Point | null = collideLineLine(line, line_prime)
-		// 		if (intersection) {
-		// 			triangle1_points.push(intersection)
-		// 			p5.strokeWeight(5)
-		// 			p5.stroke('red')
-		// 			p5.point(...intersection)
-		// 		}
-		// 	}
-		// })
-		// if (!isInside(line[1], triangles[0])) {
-		// 	triangle1_points.push(line[1])
-		// }
-		// })
-
-		// for (let i = 0; i < triangle1_points.length; i++) {
-		// 	p5.strokeWeight(5)
-		// 	switch (i) {
-		// 		case 0:
-		// 			p5.stroke('green')
-		// 			p5.point(...triangle1_points[0]!)
-		// 			break
-		// 		case 1:
-		// 			p5.stroke('red')
-		// 			p5.point(...triangle1_points[1]!)
-		// 			break
-		// 		default:
-		// 			p5.stroke('blue')
-		// 			p5.point(...triangle1_points[i]!)
-		// 			break
-		// 	}
-		// }
-
-		// for (let i = 0; i < triangle1_points.length / 2; i++) {
-		// 	p5.strokeWeight(1)
-		// 	p5.stroke('black')
-		// 	if (triangle1_points[i * 2] && triangle1_points[i * 2 + 1]) {
-		// 		p5.line(...triangle1_points[i * 2]!, ...triangle1_points[i * 2 + 1]!)
-		// 	}
-		// }
-
-		// for (let tri of triangles) {
-		// 	tri.rotate()
-		// 	tri.lines.forEach(line => {
-		// 		p5.line(...line.start, ...line.end)
-		// 	});
-		// }
-
-		// triangles[1]?.lines.forEach(line => {
-		// 	p5.line(...line.start, ...line.end)
-		// });
-
-		// if (triangles[1] && triangles[0]) {
-		// 	let intersections = triangles[0]?.findIntersections(triangles[1]) || null
-		// 	let line_0: [number, number][] = []
-		// 	let line_1: [number, number][] = []
-
-		// 	for (let obj of intersections) {
-		// 		if (obj.id === 0) {
-		// 			line_0.push(obj.point)
-		// 		} if (obj.id === 1) {
-		// 			line_1.push(obj.point)
-		// 		}
-		// 	}
-		// 	if (line_0.length === 2) {
-		// 		p5.strokeWeight(1)
-		// 		p5.stroke('red')
-		// 		if (line_0[0] && line_0[1]) {
-		// 			p5.line(line_0[0][0], line_0[0][1], line_0[1][0], line_0[1][1])
-		// 		} else if (line_0[0]) {
-		// 			p5.line(line_0[0][0], line_0[0][1], triangles[1].lines[0].end[0], triangles[1].lines[1].end[1])
-		// 		}
-
-		// 	}
-		// 	if (line_1.length === 2) {
-		// 		p5.strokeWeight(1)
-		// 		p5.stroke('red')
-		// 		if (line_1[0] && line_1[1]) {
-		// 			p5.line(line_1[0][0], line_1[0][1], line_1[1][0], line_1[1][1])
-		// 		} else if (line_1[0]) {
-		// 			p5.line(line_1[0][0], line_1[0][1], triangles[1].lines[1].end[0], triangles[1].lines[1].end[1])
-		// 		}
-
-		// 	}
-
-		// 	p5.strokeWeight(5)
-		// 	p5.stroke('red')
-		// 	intersections.forEach((intersection: { id: number, point: [number, number] }) => {
-		// 		p5.point(...intersection.point)
-		// 	})
 	}
 }
 
