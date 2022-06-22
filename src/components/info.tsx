2ebe09cb3f99f4d141ea6da1805faaafe4929fac
lewiswolf <lewiswolstanholme@gmail.com>
2022-06-22 14:01:47 +0100

    added bio and added highlight_text() mixin

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -1,7 +1,28 @@
+// dependencies
+import { useEffect, useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+import rehypeRaw from 'rehype-raw'
+
+// src
+import bio from '../config/bio.md'
+
 export default function Info(): JSX.Element {
+	const [markdown, setMarkdown] = useState<string>('')
+
+	useEffect(() => {
+		fetch(bio)
+			.then((res) => res.text())
+			.then((text) => setMarkdown(text))
+	}, [])
+
 	return (
-		<header>
-			<p>lewiswolstanholme@gmail.com</p>
-		</header>
+		<>
+			<header>
+				<p>lewiswolstanholme@gmail.com</p>
+			</header>
+			<main className='info'>
+				<ReactMarkdown children={markdown} className='bio' rehypePlugins={[rehypeRaw]} />
+			</main>
+		</>
 	)
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,3 +2,4 @@
 /// <reference types="react-scripts" />
 declare module 'maxmsp-gui'
 declare module 'react-syntax-highlighter'
+declare module '*.md'
