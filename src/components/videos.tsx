10da3bb0297abfabc823cdfdc69a1c83d1167faf
lewiswolf <lewiswolstanholme@gmail.com>
2022-11-19 20:32:19 +0000

    vimeo

--- src/components/videos.tsx
+++ src/components/videos.tsx
@@ -5,15 +5,29 @@ export default function Videos(): JSX.Element {
 	return (
 		<main className='videos'>
 			{videos_json.map((obj: VideosJSON, i: number): JSX.Element => {
-				return (
-					<iframe
-						allow='accelerometer; autoplay; encrypted-media; fullscreen; gyroscope;'
-						frameBorder={0}
-						key={i}
-						src={`https://www.youtube-nocookie.com/embed/${obj.hash}?theme=dark&color=white`}
-						title={obj.title}
-					/>
-				)
+				switch (obj.type) {
+					case 'vimeo':
+						return (
+							<iframe
+								allow='autoplay; fullscreen; picture-in-picture;'
+								frameBorder={0}
+								src={`https://player.vimeo.com/video/${obj.hash}?h=be7c17d620`}
+								title={obj.title}
+							/>
+						)
+					case 'youtube':
+						return (
+							<iframe
+								allow='accelerometer; autoplay; encrypted-media; fullscreen; gyroscope;'
+								frameBorder={0}
+								key={i}
+								src={`https://www.youtube-nocookie.com/embed/${obj.hash}?theme=dark&color=white`}
+								title={obj.title}
+							/>
+						)
+					default:
+						return <></>
+				}
 			})}
 		</main>
 	)
--- src/config/videos.ts
+++ src/config/videos.ts
@@ -1,47 +1,58 @@
 export type VideosJSON = {
 	hash: string
 	title: string
+	type: 'youtube' | 'vimeo'
 }
 
 export const videos_json: VideosJSON[] = [
 	{
-		hash: 'IpqrfoCyOmw',
+		hash: '772567653',
 		title: 'josef by Julia Set',
+		type: 'vimeo',
 	},
 	{
-		hash: 'LpO2JyVuqN4',
+		hash: '772567097',
 		title: 'terracotta by Julia Set',
+		type: 'vimeo',
 	},
 	{
 		hash: '0arhLOG00Js',
 		title: 'topspin (demo) by Julia Set',
+		type: 'youtube',
 	},
 	{
-		hash: 'H_7bpFmk4bI',
+		hash: '772565927',
 		title: 'morass by Julia Set',
+		type: 'vimeo',
 	},
 	{
-		hash: 'dVkmS5ku2jc',
+		hash: '772562898',
 		title: 'cairn by Julia Set',
+		type: 'vimeo',
 	},
 	{
 		hash: 's3jbrXySGXg',
 		title: 'weaving by Lewis Wolstanholme',
+		type: 'youtube',
 	},
 	{
 		hash: '20eLg6skiK4',
 		title: 'chroma by Lewis Wolstanholme',
+		type: 'youtube',
 	},
 	{
 		hash: 'eIMS1Q_vu_M',
 		title: 'sabar by Julia Set',
+		type: 'youtube',
 	},
 	{
 		hash: 'iXguBng4cJg',
 		title: 'marble by Julia Set',
+		type: 'youtube',
 	},
 	{
 		hash: 'sZYNHRozFBc',
 		title: 'bach.fractal by Ciarán Corr and Lewis Wolstanholme',
+		type: 'youtube',
 	},
 ]
