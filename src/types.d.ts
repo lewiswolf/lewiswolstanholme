1841f910ba638af3c1d102792d7461a64a35a518
lewiswolf <lewiswolstanholme@gmail.com>
2023-10-11 23:37:25 +0100

    types for react-syntax-highlighter

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,5 @@
 // dependencies
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
-declare module 'react-syntax-highlighter'
+// declare module 'react-syntax-highlighter'
 declare module '*.md'
