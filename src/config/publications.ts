90f992af20c0ab0dd7b4e1603993dbc9a0ff4e84
lewiswolf <lewiswolstanholme@gmail.com>
2023-11-14 18:05:54 +0000

    more data for publications.ts

--- src/config/publications.ts
+++ src/config/publications.ts
@@ -1,41 +1,41 @@
-type PublicationJSON = {
+export type PublicationJSON = {
 	authors: string[]
 	date: Date
-	permalink: string
+	links?: { [key: string]: string }
+	pdf?: string
+	permalink?: string
 	place?: string
 	publication: string
 	title: string
 	type: 'article' | 'conference paper' | 'dataset' | 'performance' | 'presentation' | 'workshop'
 }
 
-export const publication_json: PublicationJSON[] = [
-	// {
-	// 	authors: [
-	// 		'Lewis Wolstanholme',
-	// 		'Teresa Pelinski',
-	// 		'Aliénor Golvet',
-	// 		'Cyrus Vahidi',
-	// 		'Vincent Martos',
-	// 	],
-	// 	date: new Date(),
-	// 	permalink: '',
-	// 	place: 'Institut de Recherche et Coordination Acoustique/Musique, Paris',
-	// 	publication:
-	// 		'AI (r)evolution - where are we heading?: Thoughts about the future of music and sound technologies in the era of deep learning',
-	// 	title: 'Responsible Innovation and Creative Practice',
-	// 	type: 'article',
-	// },
+export const publications: PublicationJSON[] = [
 	// {
 	// 	authors: ['Lewis Wolstanholme', 'Francis Devine'],
 	// 	date: new Date(),
+	// 	pdf: '',
 	// 	permalink: '',
 	// 	publication: 'Internation Journal of Creative Media Research',
 	// 	title: 'josef: Spatiality as a Material Property of Audiovisual Art',
 	// 	type: 'article',
 	// },
 	{
+		authors: ['Lewis Wolstanholme', 'Teresa Pelinski', 'Aliénor Golvet', 'Cyrus Vahidi', 'Vincent Martos'],
+		date: new Date('2023-11-31'),
+		pdf: '',
+		permalink: 'https://doi.org/10.48550/arXiv.2310.18320',
+		place: 'Institut de Recherche et Coordination Acoustique/Musique (IRCAM), Paris',
+		publication:
+			'AI (r)evolution - where are we heading?: Thoughts about the future of music and sound technologies in the era of deep learning',
+		title: 'Responsible Innovation and Creative Practice',
+		type: 'article',
+	},
+	{
 		authors: ['Lewis Wolstanholme', 'Andrew McPherson'],
 		date: new Date('2023-09-11'),
+		links: { code: 'https://github.com/lewiswolf/kac_prediction' },
+		pdf: 'Wolstanholme_McPherson_2023_Towards_orchestrating_physically_modelled_2D_percussion_instruments',
 		permalink: '',
 		place: 'Politecnico di Torino, Italy',
 		publication: 'Forum Acusticum, 10th Convention of the European Acoustics Association',
@@ -59,7 +59,12 @@ export const publication_json: PublicationJSON[] = [
 			'Jordie Shier',
 		],
 		date: new Date('2023-08-30'),
-		permalink: '',
+		links: {
+			code: 'https://github.com/rodrigodzf/NeuralResonatorBela',
+			demo: '',
+		},
+		pdf: 'Armitage_et_al_2023_Agential_Instruments_Design_Workshop',
+		permalink: 'https://aimc2023.pubpub.org/pub/25mg4xnz',
 		place: 'University of Sussex, UK',
 		publication: '4th International Conference on AI and Music Creativity (AIMC)',
 		title: 'Agential Instruments Design Workshop',
@@ -68,6 +73,11 @@ export const publication_json: PublicationJSON[] = [
 	{
 		authors: ['Lewis Wolstanholme', 'Cyrus Vahidi', 'Andrew McPherson'],
 		date: new Date('2023-08-23'),
+		links: {
+			code: 'https://github.com/lewiswolf/hearing-from-within-a-sound',
+			demo: 'https://youtu.be/-0i4IlHmgRs',
+		},
+		pdf: 'Wolstanholme_et_al_2023_Hearing_from_within_a_sound',
 		permalink: 'http://www.aes.org/e-lib/browse.cfm?elib=22158',
 		place: 'University of Huddersfield, UK',
 		publication: 'International Conference on Spatial and Immersive Audio (AES)',
@@ -77,6 +87,7 @@ export const publication_json: PublicationJSON[] = [
 	{
 		authors: ['Lewis Wolstanholme', 'Andrew McPherson'],
 		date: new Date('2022-12-20'),
+		pdf: 'Wolstanholme_McPherson_2022_Remarks_on_a_Cultural_Investigation_of_Abstract_Percussion_Instruments',
 		permalink: 'https://www.qmul.ac.uk/dmrn/media/dmrn/DMRN-17-proceedings---Draft.pdf',
 		place: 'Queen Mary University of London, UK',
 		publication: '17th Digital Music Research Network (DMRN)',
@@ -86,6 +97,7 @@ export const publication_json: PublicationJSON[] = [
 	{
 		authors: ['Lewis Wolstanholme'],
 		date: new Date('2022-11-23'),
+		links: { code: 'https://github.com/lewiswolf/kac_drumset' },
 		permalink: 'https://doi.org/10.5281/zenodo.7274474',
 		publication: 'Zenodo',
 		title: 'kac_drumset: A Dataset Generator for Arbitrarily Shaped Drums',
@@ -94,6 +106,8 @@ export const publication_json: PublicationJSON[] = [
 	{
 		authors: ['Lewis Wolstanholme', 'Francis Devine'],
 		date: new Date('2022-06-22'),
+		links: { watch: 'https://vimeo.com/772567097' },
+		pdf: 'Wolstanholme_Devine_2022_terracotta',
 		permalink: 'https://doi.org/10.21428/92fbeb44.7c140077',
 		place: 'University of Auckland, New Zealand',
 		publication: '22nd International Conference on New Interfaces for Musical Expression (NIME)',
@@ -103,7 +117,7 @@ export const publication_json: PublicationJSON[] = [
 	{
 		authors: ['Lewis Wolstanholme', 'Francis Devine'],
 		date: new Date('2022-06-16'),
-		permalink: '',
+		links: { watch: 'https://youtu.be/d6IfdD9n2A4' },
 		place: 'University of Gloucestershire, UK',
 		publication: 'Everyday is Spatial: Immersive Audio Conference',
 		title: 'Subjective Spatial Colour',
