6cceed02d91ecd380db4dc39ea3c587f830f4a71
lewiswolf <lewiswolstanholme@gmail.com>
2024-01-02 10:36:20 +0000

    performance boost

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import { compression } from 'vite-plugin-compression2'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	assetsInclude: ['**/*.md'],
+	build: { sourcemap: true, target: 'ESNext' },
 	plugins: [
 		compression({
 			algorithm: 'gzip',
-			include: /\.(js|mjs|json|css|svg)$/i,
+			include: /\.(js|map|mjs|json|css|svg)$/i,
 		}),
 		react(),
+		splitVendorChunkPlugin(),
 		svgr(),
 	],
 })
