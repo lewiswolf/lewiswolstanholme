fe4fe7a76c54f662fc43e1651a747a4f03ce119a
lewiswolf <lewiswolstanholme@gmail.com>
2024-02-03 13:07:49 +0000

    removed the over engineered metadata stuff

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,9 @@
 // dependencies
 import { useEffect } from 'react'
-import { HelmetProvider } from 'react-helmet-async'
+import { Helmet } from 'react-helmet-async'
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 
 // components
-import Metadata from './sub-components/metadata'
 import { GoogleAnalytics, analyticsPageView } from './analytics'
 import Navi from './navi'
 import Home from './home'
@@ -24,13 +23,12 @@ export default function App(): JSX.Element {
 	}, [location])
 
 	return (
-		<HelmetProvider>
-			<Metadata
-				description='Lewis Wolstanholme - Composer & Artist'
-				title={`Lewis Wolstanholme${
+		<>
+			<Helmet>
+				<title>{`Lewis Wolstanholme${
 					location === '/' ? '' : ` | ${location.slice(1, 2).toUpperCase()}${location.slice(2)}`
-				}`}
-			/>
+				}`}</title>
+			</Helmet>
 			<GoogleAnalytics id='G-52FG4N6KPP' />
 			<Navi />
 			<Routes>
@@ -42,6 +40,6 @@ export default function App(): JSX.Element {
 				<Route element={<Info />} path='/info' />
 				<Route element={<Navigate to='/' />} path='*' />
 			</Routes>
-		</HelmetProvider>
+		</>
 	)
 }
