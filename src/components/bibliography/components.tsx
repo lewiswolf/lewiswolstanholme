7a042dd8378efa4b2c194a81ba0a7223b2461824
lewiswolf <lewiswolstanholme@gmail.com>
2024-07-07 19:01:20 +0100

    type errors

--- src/components/bibliography/components.tsx
+++ src/components/bibliography/components.tsx
@@ -5,26 +5,32 @@ export function parseCitation(P: PublicationJSON[string]): JSX.Element {
 	let authors = ''
 	function initials(N: string): string {
 		return N.split(' ')
-			.map((n: string) => `${n[0]}.`)
+			.map((n: string) => (n[0] ? `${n[0]}.` : ''))
 			.join(' ')
 	}
 	switch (P.authors.length) {
 		case 0:
 			break
 		case 1:
-			authors = `${P.authors[0]?.last_name}, ${initials(P.authors[0]?.first_name as NonNullable<string>)} `
+			if (P.authors[0]) {
+				authors = `${P.authors[0].last_name}, ${initials(P.authors[0].first_name)} `
+			}
 			break
 		case 2:
-			authors = `${P.authors[0]?.last_name}, ${initials(P.authors[0]?.first_name as NonNullable<string>)} & ${P.authors[1]?.last_name}, ${initials(P.authors[1]?.first_name as NonNullable<string>)} `
+			if (P.authors[0] && P.authors[1]) {
+				authors = `${P.authors[0].last_name}, ${initials(P.authors[0].first_name)} & ${P.authors[1].last_name}, ${initials(P.authors[1].first_name)} `
+			}
 			break
 		default:
-			authors = `${P.authors[0]?.last_name}, ${initials(P.authors[0]?.first_name as NonNullable<string>)} et al. `
+			if (P.authors[0]) {
+				authors = `${P.authors[0].last_name}, ${initials(P.authors[0].first_name)} et al. `
+			}
 			break
 	}
 	return (
 		<p className='citation' key={P.title}>
 			{authors}
-			{P.date && '(' + P.date.getFullYear() + '). '}
+			{'(' + P.date.getFullYear().toString() + '). '}
 			{P.title && P.title + '. '}
 			{P.type !== '' && P.type !== 'article' && P.type !== 'dataset' ? 'In ' : ''}
 			{P.publication && <i>{P.publication + '. '}</i>}
